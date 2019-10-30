40729af2e151e725088879f7036ac46a7537b5c8
alexandre wagner <alexandre1.wagner@epitech.eu>
2019-10-30 21:54:38 +0100

    changemement du nom par XXX

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 from PIL import Image
 
-Image.open("ch18.png").split()[2].show()
-blue = Image.open("ch18.png").split()[2]
+name = XXX # remplace the XXX by your file
+
+Image.open(name).split()[2].show()
+blue = Image.open(name).split()[2]
 blue.point(lambda x: (256-x)**2).show()
