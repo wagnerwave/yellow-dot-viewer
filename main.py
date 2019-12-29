4aae42e674e62cd4d3fa1228bab725f806f5c45e
alexandre wagner <alexandre1.wagner@epitech.eu>
2019-12-29 22:52:16 +0100

    gestion of arguments

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
 from PIL import Image
+import sys
 
-name = XXX # remplace the XXX by your file
+if len(sys.argv) < 2:
+    print("Error:")
+    print("./main.py image.png")
+    exit()
+    
+name = sys.argv[1]
 
 Image.open(name).split()[2].show()
 blue = Image.open(name).split()[2]
